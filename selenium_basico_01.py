316e1ffa8780d07e5e949c7acd35707af29bb26c
Anderson de Oliveira Lima FIlho <anderson.filho@parvi.local>
2024-07-03 14:23:19 -0300

    primeiro_commit

---
+++ selenium_basico_01.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+Service = Service()
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=Service,options=options)
