d326489f08aeba2cb44f3aaef2421615a0f31b53
Anderson Filho <anderson.filho@parvi.local>
2024-08-02 14:54:12 -0300

    semi_final

--- aula_24/rpa_aula24.py
+++ aula_24/rpa_aula24.py
@@ -25,29 +25,48 @@ for i in dicionario_loja:
     if i not in list_nome:
        novaP = caminho / i
        novaP.mkdir()
-    nomeArq ='{}_{}_{}.xlsx'.format(dia_indicador.month,dia_indicador.day,i)
+    nomeArq =f'{dia_indicador.month}_{dia_indicador.day}_{i}.xlsx'
     print(nomeArq)
     local_arquivo = caminho/i/nomeArq
     
     dicionario_loja[i].to_excel(local_arquivo)
 
+
 #Calcular o primeiro indicador da loja 
-vendas_loja = dicionario_loja['Bourbon Shopping SP']
+loja = 'Bourbon Shopping SP'
+vendas_loja = dicionario_loja[loja]
 vendas_loja_dia = vendas_loja.loc[vendas_loja['Data']==dia_indicador,:]
-#faturamento 
+print(vendas_loja)
+#faturamento  
 faturamento_ano= vendas_loja['Valor Final'].sum()
 print(faturamento_ano)
 faturamento_dia =vendas_loja_dia['Valor Unitário'].sum() 
 print(faturamento_dia)
-
 #diversidade de produtos 
 qtd_produtosAnos = len(vendas_loja['Produto'].unique())
 print(qtd_produtosAnos)
 qtd_produtosDia = len(vendas_loja_dia['Produto'].unique())
 print(qtd_produtosDia)
+#Ticket medio
+valor_venda = vendas_loja.groupby('Código Venda')[['Produto','Quantidade', 'Valor Unitário', 'Valor Final']].sum()
+ticketMedioAno = valor_venda['Valor Final'].mean()
+ 
+#Definição de metas
+meta_faturamento_dia = 1000
+meta_faturamento_dia = 1650000
+meta_faturamento_dia = 4
+meta_faturamento_dia = 120
+meta_faturamento_dia = 500
 
-#Ticket medio e Ticket dia
-valor_venda = vendas_loja.groupby('Código Venda' ).sum()
+#Email
+import yagmail
+usuario = yagmail.SMTP('anderson.limafo@ufrpe.br','Ufrpe123')
+#para mandar email 
+nome = email.loc[email['Loja']==loja,'Gerente'].values[0]
+email = email.loc[email['Loja']==loja,'E-mail'].values[0]
+assunto = f'OnePage dia {dia_indicador.day / dia_indicador.month} - Loja {loja}'
+bc = f'{dia_indicador.month}_{dia_indicador.day}_{loja}.xlsx'
+caminhoA = str(caminho/loja/bc)
+usuario.send(to=email,subject=assunto,contents='resultado do dia' ,attachments=caminhoA,)
 
-ticketMedioAno = valor_venda['Valor Final'].mena()
-print(ticketMedioAno)
+
